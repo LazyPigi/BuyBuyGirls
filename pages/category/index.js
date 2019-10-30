6079074db935990af377148fafa9a395c382a7f9
Will-Z-fighting <53569286+Will-Z-fighting@users.noreply.github.com>
2019-10-31 02:20:15 +0800

    左侧菜单栏布局及点击高亮完成

---
+++ pages/category/index.js
@@ -0,0 +1,30 @@
+// pages/category/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+    // 当前选中的索引
+    current: 0,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  // 点击左侧的菜单时候触发
+  handleChange(event) {
+    // 获取index
+    const { index } = event.target.dataset;
+
+    this.setData({
+      current: index
+    })
+  }
+  
+})
